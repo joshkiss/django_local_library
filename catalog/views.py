030d4e728643640fef83b63c51d74747e6f526ea
joshkiss <joshk7998@gmail.com>
2024-03-28 21:23:09 -0400

    Book detail page

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views import generic 
 from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
@@ -24,3 +27,28 @@ def index(request):
 
     return render(request, 'index.html', context=context)
 
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    paginate_by = 2
+    
+    # template_name = 
+    # queryset = Book.objects.filter(genre__icontains='language')[:5]
+    # template_name = 'books/my_arbitrary_template_name_list.html'
+    
+    # def get_queryset(self) -> QuerySet[Any]:
+    #     return Book.objects.filter(title__icontains='war')[:5]
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'this is just some data'
+        return context
+    
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
